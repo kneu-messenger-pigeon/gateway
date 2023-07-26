eb417bce8c1f40e0ee665a9c458d0ae748559e2d
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-26 15:57:53 +0300

    Nginx gateway build

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ COPY nginx-reload-ssl.sh /docker-entrypoint.d/40-nginx-reload-ssl.sh
 # Copy the nginx configuration file
 COPY default.conf  /etc/nginx/conf.d/default.conf
 
-HEALTHCHECK --interval=15s --timeout=3s --start-period=2s \
+HEALTHCHECK --interval=15s --timeout=3s --start-period=5s \
   CMD wget --no-verbose --tries=1 --spider http://localhost/healthcheck || exit 1
--- test-docker.sh
+++ test-docker.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env sh
 set -e
 NAME=test-nginx
-docker rm -f $NAME || true
+docker rm -f $NAME 2>/dev/null || true
 
 sed -i.bak 's/--interval=[^ ]*/--interval=1s/g' Dockerfile
-docker build -t $NAME .
 
+trap 'rm Dockerfile; mv Dockerfile.bak Dockerfile; docker rm -f $NAME > /dev/null' EXIT HUP INT QUIT PIPE TERM
+
+docker build -t $NAME .
 docker run -d --rm --add-host authorizer:127.0.0.1 --add-host host.docker.internal:host-gateway --name $NAME $NAME
-trap "docker rm -f $NAME" EXIT HUP INT QUIT PIPE TERM
+
+docker logs $NAME
+docker logs $NAME 2>&1 | grep "Use default ssl certificates from /ssl-default"
 
 docker exec $NAME nginx -t
 
 sleep 1
-docker ps -f name=$NAME -f health=healthy | grep $NAME
-docker down $NAME
+HEALTHY=$(docker ps -f name=$NAME -f health=healthy --quiet | wc -l)
+if [ "$HEALTHY" -eq 1 ]; then
+  echo "Container $NAME is healthy"
+else
+  echo "Container $NAME is not healthy"
+  exit 1
+fi
