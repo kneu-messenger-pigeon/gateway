87f5137e648711fc78e5047908b2ee3d162b0455
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-26 16:09:48 +0300

    Add debug

--- test-docker.sh
+++ test-docker.sh
@@ -3,19 +3,23 @@ set -e
 NAME=test-nginx
 docker rm -f $NAME 2>/dev/null || true
 
-sed -i.bak 's/--interval=[^ ]*/--interval=1s/g' Dockerfile
+sed -i.bak 's/--interval=[^ ]*/--interval=2s/g' Dockerfile
 
-trap 'rm Dockerfile; mv Dockerfile.bak Dockerfile; docker rm -f $NAME > /dev/null' EXIT HUP INT QUIT PIPE TERM
+trap 'echo "exit - $?"; rm Dockerfile; mv Dockerfile.bak Dockerfile; docker rm -f $NAME' EXIT HUP INT QUIT PIPE TERM
 
 docker build -t $NAME .
 docker run -d --rm --add-host authorizer:127.0.0.1 --add-host host.docker.internal:host-gateway --name $NAME $NAME
 
-docker logs $NAME
+echo "Container run success"
+docker ps -a
+
+sleep 2
+
+docker logs $NAME 2>&1
 docker logs $NAME 2>&1 | grep "Use default ssl certificates from /ssl-default"
 
 docker exec $NAME nginx -t
 
-sleep 1
 HEALTHY=$(docker ps -f name=$NAME -f health=healthy --quiet | wc -l)
 if [ "$HEALTHY" -eq 1 ]; then
   echo "Container $NAME is healthy"
