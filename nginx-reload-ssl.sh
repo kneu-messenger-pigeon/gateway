d09610d86aa7f9ce89e3137ee0d429e8d5303f70
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-26 08:02:20 +0300

    Improve work with ssl certificate

--- nginx-reload-ssl.sh
+++ nginx-reload-ssl.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env sh
-mkdir -p /ssl
 
-[ -f /ssl/fullchain.pem ] || cp /ssl-default/fullchain.pem /ssl/fullchain.pem
-[ -f /ssl/privkey.pem ] || cp /ssl-default/privkey.pem /ssl/privkey.pem
-[ -f /ssl/dhparam.pem ] || cp /ssl-default/dhparam.pem /ssl/dhparam.pem
+mkdir -p /ssl
+set -e
+
+[ -L /ssl-enabled ] && rm /ssl-enabled
+
+if [ -f /ssl/fullchain.pem ] && [ -f /ssl/privkey.pem ]; then
+  echo "Use custom ssl certificates from /ssl"
+  ln -s /ssl /ssl-active
+else
+  echo "Use default ssl certificates from /ssl-default"
+  ln -s /ssl-default /ssl-active
+fi
+
+if [ -f /ssl/dhparam.pem ]; then
+  echo "Use custom dhparam.pem from /ssl"
+  ln -s /ssl/dhparam.pem /dhparam.pem
+else
+  echo "Use default dhparam.pem from /ssl-default"
+  ln -s /ssl-default/dhparam.pem /dhparam.pem
+fi
 
 # reload nginx every day - this is a workaround for reload ssl certificates after update
 nohup /bin/sh -c 'while :; do sleep 1d; nginx -t && nginx -s reload; done' >/dev/null 2>&1 &
